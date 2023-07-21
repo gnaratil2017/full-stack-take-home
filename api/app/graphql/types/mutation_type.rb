7906ba2f78d8751a35563cd53a43ced80ea0acd5
gnaratil2017 <greg.naratil@gmail.com>
2023-07-21 15:04:40 -0400

    add toggle chatroom resolved mutation to api

--- api/app/graphql/types/mutation_type.rb
+++ api/app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_chatroom, mutation: Mutations::CreateChatroom
     field :update_chatroom_description, mutation: Mutations::UpdateChatroomDescription
+    field :toggle_chatroom_resolved, mutation: Mutations::ToggleChatroomResolved
   end
 end
